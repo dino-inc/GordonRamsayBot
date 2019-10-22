a0fb7235e9786f4879e0aee20c899ee513e237be
dino-inc <ethan8960@gmail.com>
2019-10-22 00:04:15 -0700

    Setting up the config cog part 2

--- cogs/config.py
+++ cogs/config.py
@@ -1,25 +1,30 @@
 import discord
 from discord.ext import commands
 import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 
 class Config(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    #The config group
-    @commands.group(name = "Config",
-                    invoke_without_command=True,
-                    help = "The group of config commands.")
+        # Load the cog commands into the bot.
+        # for command in Config.get_commands(self):
+
+    # The config group of commands.
+    @commands.group(name="config",
+               invoke_without_command=True,
+               help="The group of config commands.")
     @commands.is_owner()
     async def config(self, ctx):
-        await ctx.send("testing invocation without subcommand")
+        await ctx.send("You should check your parameters, chef.")
 
-    #sets the number of upvotes for best_of
-    @commands.command(name="set_upvotes",
+    # Sets the number of upvotes for best_of.
+    @config.command(name="set_upvotes",
                       help = "Sets the number of upvotes required for best_of in the server",
                       brief = "Sets upvotes for the memes channel.")
     @commands.is_owner()
     async def set_upvotes(self, ctx, upvotes):
         await ctx.send("upvotes... set. Pretend they did.")
+
 
 
 def setup(bot):
--- gordonramsay.py
+++ gordonramsay.py
@@ -12,7 +12,7 @@ from sqlalchemy import Column, Integer, String
 
 
 # SQL Alchemy config
-engine = create_engine('sqlite:///database.db', echo=True)
+engine = create_engine('sqlite:///database.db', echo=False)
 Base = declarative_base()
 
 
@@ -61,23 +61,24 @@ bot = commands.Bot(command_prefix=';')
 @bot.event
 async def on_ready():
     print(f"Gordon Ramsay is online on {bot.user.name}, id {bot.user.id}.")
+    # Load startup cogs
+    initial_extensions = ['cogs.config']
+    if __name__ == '__main__':
+        for extension in initial_extensions:
+            print(f'Loading {extension}.')
+            try:
+                bot.load_extension(extension)
+                print('Finished loading.')
+            except Exception as e:
+                print(f'Failed to load extension {extension}.', file=sys.stderr)
+                traceback.print_exc()
 
-# Load startup cogs
-initial_extensions=['cogs.config']
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        print(f'Loading {extension}.')
-        try:
-            bot.load_extension(extension)
-            print('Finished loading.')
-        except Exception as e:
-            print(f'Failed to load extension {extension}.', file=sys.stderr)
-            traceback.print_exc()
 
 # Process the commands from each message input
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
+
 
 # Run the bot using the bot token
 token = open("token.txt", 'r')

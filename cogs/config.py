cd92700118c8d0afe525f145f6a0ff77c7fcde11
dino-inc <ethan8960@gmail.com>
2019-12-17 23:49:52 -0800

    Added set_global_var and made the commands less ID dependent

--- cogs/config.py
+++ cogs/config.py
@@ -19,7 +19,7 @@ class Global(Base):
     downvote_emoji_id = Column(Integer)
 
     def __repr__(self):
-        return "Global:\n<owner_id='%s',\n upvote_emoji_id='%s',\ndownvote_emoji_id='%s'>" % (
+        return "Global:\nowner_id='%s'\nupvote_emoji_id='%s'\ndownvote_emoji_id='%s'" % (
                 self.owner_id, self.upvote_emoji_id, self.downvote_emoji_id)
 
 
@@ -38,8 +38,8 @@ class Server(Base):
     downvotes = Column(Integer)
 
     def __repr__(self):
-        return "Server:\nid='%s'\n shitposting_id='%s'\n memes_id='%s'\n worst_of_id='%s'\n best_of_id='%s'\n" \
-               " mod_log_id='%s'\n stars='%s'\n downstars='%s'\n upvotes='%s'\n downvotes='%s'" % (
+        return "Server:\nid='%s'\nshitposting_id='%s'\n memes_id='%s'\nworst_of_id='%s'\nbest_of_id='%s'\n" \
+               "mod_log_id='%s'\nstars='%s'\ndownstars='%s'\nupvotes='%s'\ndownvotes='%s'" % (
                 self.id, self.shitposting_id, self.memes_id, self.worst_of_id, self.best_of_id, self.mod_log_id,
                 self.stars, self.downstars, self.upvotes, self.downvotes)
 
@@ -76,26 +76,40 @@ class Config(commands.Cog):
 
     @config.command()
     @commands.is_owner()
-    async def set_server_var(self, ctx, type_name, filter_attr, var_name, var_value):
+    async def set_server_var(self, ctx, var_name, var_value):
         session = self.Session()
-        server_vars = session.query(type_name).filter_by(id=ctx.guild.id).first()
+        server_vars = get_server_ob(ctx, session)
         old_value = getattr(server_vars, f"{var_name}")
         setattr(server_vars, f"{var_name}", var_value)
         await ctx.send(f"Changed the value from {old_value} to {getattr(server_vars, f'{var_name}')}.")
-        print(server_vars)
+        session.commit()
+        session.close()
+
+    @config.command()
+    @commands.is_owner()
+    async def set_global_var(self, ctx, var_name, var_value):
+        session = self.Session()
+        global_vars = session.query(Global).filter_by(owner_id=ctx.author.id).first()
+        old_value = getattr(global_vars, f"{var_name}")
+        setattr(global_vars, f"{var_name}", var_value)
+        await ctx.send(f"Changed the value from {old_value} to {getattr(global_vars, f'{var_name}')}.")
+        session.commit()
         session.close()
 
     @config.command()
     @commands.is_owner()
     async def channels(self, ctx):
-        channels = ['shitposting', 'memes', 'worst_of', 'best_of', 'mod_log']
+        # channels
+        channels = ['shitposting_id', 'memes_id', 'worst_of_id', 'best_of_id', 'mod_log_id',
+                    # not channels
+                    'stars', 'downstars', 'upvotes', 'downvotes']
         session = self.Session()
         server_vars = get_server_ob(ctx, session)
-        await multi_user_input(ctx, self, session, channels, "channel ID")
+        await multi_user_input(ctx, self, session, server_vars, channels)
         verification_msg = ""
-        for channel_name in channels:
-            id_attr = getattr(server_vars, f"{channel_name}_id")
-            verification_msg += f"{channel_name}: `{id_attr}`\n"
+        for value_name in channels:
+            id_attr = getattr(server_vars, f"{value_name}")
+            verification_msg += f"{value_name}: `{id_attr}`\n"
         await ctx.send(verification_msg)
         session.commit()
         session.close()
@@ -103,13 +117,13 @@ class Config(commands.Cog):
     @config.command()
     @commands.is_owner()
     async def globals(self, ctx):
-        global_vars = ['owner', 'upvote_emoji', 'downvote_emoji']
+        globals = ['owner_id', 'upvote_emoji_id', 'downvote_emoji_id']
         session = self.Session()
-        server_vars = get_server_ob(ctx, session)
-        await multi_user_input(ctx, self, session, global_vars, "global variable")
+        global_vars = session.query(Global).filter_by(owner_id=ctx.author.id).first()
+        await multi_user_input(ctx, self, session, global_vars, globals)
         verification_msg = ""
-        for var_name in global_vars:
-            id_attr = getattr(server_vars, f"{var_name}_id")
+        for var_name in globals:
+            id_attr = getattr(global_vars, f"{var_name}_id")
             verification_msg += f"{var_name}: `{id_attr}`\n"
         await ctx.send(verification_msg)
         session.commit()
@@ -127,26 +141,25 @@ def get_server_ob(ctx, session):
     return session.query(Server).filter_by(id=ctx.guild.id).first()
 
 
-async def multi_user_input(ctx, self, session, item_array, data_type_name):
-    # Ensures the responding user matches the one who started the command
+async def multi_user_input(ctx, self, session, datatype_vars, item_array):
+    # Ensures the responding user matches the one who started the command and is in the same channel
     def verify_user(message):
-        if message.author == ctx.message.author:
+        if message.author == ctx.message.author and message.channel == ctx.message.channel:
             return True
         else:
             return False
 
-    server_vars = get_server_ob(ctx, session)
     composite_msg = ""
     for item_name in item_array:
-        composite_msg += f"What is the {item_name} {data_type_name}?"
+        composite_msg += f"What is the {item_name} value?"
         await ctx.send(composite_msg)
         try:
             choice = await self.bot.wait_for('message', check=verify_user, timeout=30)
         except asyncio.TimeoutError:
-            await ctx.send(f"No input found, exiting {data_type_name} config.")
+            await ctx.send(f"No input found, exiting value config.")
             session.close()
             return
-        setattr(server_vars, f"{item_name}_id", choice.content)
+        setattr(datatype_vars, f"{item_name}", choice.content)
         composite_msg = f"Set {item_name}'s ID to `{choice.content}`\n"
     await ctx.send(composite_msg)
 
